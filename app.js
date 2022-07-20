5f80cdde9b0ba9c5029bb7eb4cdd1c1fb09426da
DarioKnezovic <dknezovic997@gmail.com>
2022-07-20 20:28:02 +0200

    Started to work on api call interval.

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
 const express = require('express');
 const server = require('./config/server');
 const currency = require('./services/currency');
+
 // App
 const app = express();
 app.get('/', (req, res) => {
     res.send('Server loaded');
 });
 
-currency.fetchCurrencyRatesFromApi()
+//currency.getCurrencyRates()
 
 app.listen(server.PORT, server.HOST);
 console.log(`Running on http://${server.HOST}:${server.PORT}`);
--- services/currency.js
+++ services/currency.js
@@ -1,13 +1,31 @@
 const axios = require('axios');
 const api = require('../config/api');
 
+const fetchCurrencyRatesFromApi = () => {
+    console.log("HEEEJ");
+    const options = {
+        method: 'GET',
+        url: api.RAPID_API_BASE_URL + '/rates',
+        headers: {
+            'X-RapidAPI-Key': api.RAPID_API_KEY,
+            'X-RapidAPI-Host': api.RAPID_API_HOST
+        }
+    };
+
+    axios.request(options)
+        .then((response) => {
+            console.log(response.data)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+    return setInterval(fetchCurrencyRatesFromApi, api.API_CALL_INTERVAL);
+}
+
+const getCurrencyRates = () => {
+    fetchCurrencyRatesFromApi();
+}
+
 module.exports = {
-    fetchCurrencyRatesFromApi: () => {
-        /*setInterval(() => {
-            axios.get(api.RAPID_API_BASE_URL + '/rates')
-                .then((response) => {
-                    console.log(response.data)
-                })
-        }, api.API_CALL_INTERVAL);*/
-    }
+    getCurrencyRates: getCurrencyRates,
 }
